731b2c2d24d0c67b4e878d9a58ddc40c178e1c16
umar <unzippinglinux@gmail.com>
2021-02-07 14:56:38 +0600

    onclick apprearance

--- script.js
+++ script.js
@@ -4,6 +4,10 @@
 // https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
 
 document.getElementById('btnClicked').addEventListener('click', ()=> {
+    // document.querySelectorAll('.card').forEach(card => {
+    //     card.style.display = none;
+    // })
+
     const searchedItem = document.getElementById('searchedItem').value;
     const fetchLink = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchedItem;
 
@@ -12,31 +16,68 @@ document.getElementById('btnClicked').addEventListener('click', ()=> {
     .then(data => foodName(data));
 
     // Following section will be used for template literal
-    const foodName = inputArray => {
+    const foodName = apiObject => {
         const cardsDiv = document.getElementById('cardsDiv')
-        inputArray.meals.forEach(meal => {
+        for (let i = 0; i < apiObject.meals.length; i++) {
+            const meal = apiObject.meals[i];
             //getting the name and image of the food in html
             const singleFoodName = meal.strMeal;
             const singleFoodImage = meal.strMealThumb;
             const singleCardInfo = `
                                     <img src="${singleFoodImage}" alt="">
                                     <h3>${singleFoodName}</h3>
-            `
+            `;
             // adding the html in cards showcase
             const singleCard = document.createElement('div');
             singleCard.className = 'card';
+            singleCard.id = 'card-no-' + i;
+            singleCard.onclick = showDetails;
             singleCard.innerHTML = singleCardInfo;
             cardsDiv.appendChild(singleCard);
-        });
+            
+        }
     }
 })
 
 
-
-
+function showDetails(){
+    document.getElementById('foodDetailsDiv').style.display = 'block';
+    console.log(this.id);
+}
 
 
 // testing purpose data
 // fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=tom')
 // .then(response => response.json())
 // .then(data => console.log(data));
+
+
+// document.getElementById('btnClicked').addEventListener('click', ()=> {
+//     const searchedItem = document.getElementById('searchedItem').value;
+//     const fetchLink = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchedItem;
+
+//     fetch(fetchLink)
+//     .then(response => response.json())
+//     .then(data => foodName(data));
+
+//     // Following section will be used for template literal
+//     const foodName = inputArray => {
+//         const cardsDiv = document.getElementById('cardsDiv')
+//         inputArray.meals.forEach(meal => {
+//             //getting the name and image of the food in html
+//             const singleFoodName = meal.strMeal;
+//             const singleFoodImage = meal.strMealThumb;
+//             const singleCardInfo = `
+//                                     <img src="${singleFoodImage}" alt="">
+//                                     <h3>${singleFoodName}</h3>
+//             `
+//             // adding the html in cards showcase
+//             const singleCard = document.createElement('div');
+//             singleCard.className = 'card';
+//             singleCard.innerHTML = singleCardInfo;
+//             cardsDiv.appendChild(singleCard);
+//         });
+//     }
+// })
+
+
